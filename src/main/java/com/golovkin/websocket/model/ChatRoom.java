794cd64b1d498723eef33641d08b7ba9a49e6932
Segrey G. <emelyanov.sm2002@gmail.com>
2024-11-27 21:59:23 +0300

    add chatroom model

---
+++ src/main/java/com/golovkin/websocket/model/ChatRoom.java
@@ -0,0 +1,22 @@
+package com.golovkin.websocket.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@Document
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatRoom {
+
+    @Id
+    private String id;
+
+    private String chatId;
+    private String sederId;
+    private String recipientId;
+
+}
